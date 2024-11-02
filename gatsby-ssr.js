e9f9720ddda7b1e0322d4eef63839212738e7738
KUROSHITA Hideaki <hkuroshi@gmail.com>
2024-11-02 16:12:44 +0900

    refactor: remove unused HTML and JavaScript files to clean up the project

--- gatsby-ssr.js
+++
@@ -1,7 +0,0 @@
-const React = require("react")
-
-exports.wrapRootElement = ({ element, props }) => (
-  <React.StrictMode {...props}>
-    <SSRProvider>{element}</SSRProvider>
-  </React.StrictMode>
-)
