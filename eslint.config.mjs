386b4bb499a12ed946032f337c755c3f707fac70
KUROSHITA Hideaki <hkuroshi@gmail.com>
2024-11-03 15:28:40 +0900

    refactor: reorganize Prettier and ESLint configurations for gatsby-theme-spin-one

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,12 @@
 // @ts-check
 
-import eslint from '@eslint/js'
-import tsEslint from 'typescript-eslint'
-import eslintConfigPrettier from 'eslint-config-prettier'
+import eslint from '@eslint/js';
+import tsEslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
-export default tsEslint.config(
-  eslint.configs.recommended,
-  ...tsEslint.configs.recommended,
-  eslintConfigPrettier,
-  {
-    rules: {
-      'no-undef': 'off',
-    },
-    files: ['**/*.ts', '**/*.mjs'],
-  }
-)
+export default tsEslint.config(eslint.configs.recommended, ...tsEslint.configs.recommended, eslintConfigPrettier, {
+  rules: {
+    'no-undef': 'off',
+  },
+  files: ['**/*.ts', '**/*.mjs'],
+});
---
+++ packages/gatsby-theme-spin-one/README.md
@@ -0,0 +1,1 @@
+# Gatsby Theme for SPIN ONE
