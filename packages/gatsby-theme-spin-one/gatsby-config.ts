642c8030727926b6ea701575106401e96bc79600
KUROSHITA Hideaki <hkuroshi@gmail.com>
2025-01-02 20:10:56 +0900

    refactor: remove enableTags option from Contentful configuration

--- packages/gatsby-theme-spin-one/gatsby-config.ts
+++ packages/gatsby-theme-spin-one/gatsby-config.ts
@@ -8,7 +8,6 @@ const contentfulConfig = {
   spaceId: process.env.CONTENTFUL_SPACE_ID as string,
   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN as string,
   environment: process.env.CONTENTFUL_ENVIRONMENT_ID || 'master',
-  enableTags: true,
   pageLimit: 50,
 };
 
