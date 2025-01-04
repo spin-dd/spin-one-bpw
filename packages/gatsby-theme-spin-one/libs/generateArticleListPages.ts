2e275f0f8037398f20021cf6cb76baa758c680a9
KUROSHITA Hideaki <hkuroshi@gmail.com>
2025-01-04 16:49:20 +0900

    refactor: restructure article list page generation to separate handling for types and categories

--- packages/gatsby-theme-spin-one/libs/generateArticleListPages.ts
+++ packages/gatsby-theme-spin-one/libs/generateArticleListPages.ts
@@ -31,19 +31,6 @@ export const generateArticleListPages = async ({ graphql, actions }, themeOption
     }
   `);
 
-  // locale を含まない記事ページの pagePath を生成
-  const createCanonicalPath = ({ type, category, page }) => {
-    const basePath = `/${type.slug}/${category.slug}/`;
-    return page === 1 ? basePath : `${basePath}${page}/`;
-  };
-  const createPagePath = ({ node_locale, type, category, page }) =>
-    `${resolveLocalePath(node_locale, defaultLocaleCode)}${createCanonicalPath({
-      type,
-      category,
-      page,
-    })}`;
-
-  // ArticleType[]、ArticleCategory[]をループしてページ生成する
   const types = result.data.allContentfulArticleType.nodes;
   const categories = result.data.allContentfulArticleCategory.nodes;
   if (types.length === 0 || categories.length === 0) {
@@ -51,8 +38,77 @@ export const generateArticleListPages = async ({ graphql, actions }, themeOption
     return;
   }
 
+  // ArticleType[]、ArticleCategory[]をループして記事一覧ページを生成する
   for (const type of types) {
+    // ArticleCategoryなしの記事一覧ページを生成
+    const createCanonicalPath = ({ type, page }) => {
+      const basePath = `/${type.slug}/`;
+      return page === 1 ? basePath : `${basePath}${page}/`;
+    };
+    const createPagePath = ({ node_locale, type, page }) =>
+      `${resolveLocalePath(node_locale, defaultLocaleCode)}${createCanonicalPath({ type, page })}`;
+
+    const articles = await graphql(`
+      {
+        allContentfulArticle(
+          filter: { type: { slug: { eq: "${type.slug}" } }, node_locale: { eq: "${type.node_locale}" } }
+        ) {
+          nodes {
+            contentful_id
+            __typename
+            node_locale
+            slug
+          }
+          totalCount
+        }
+      }
+    `);
+    const numPages = Math.ceil(articles.data.allContentfulArticle.totalCount / articlesPerPage);
+    Array.from({ length: numPages }).forEach((_, i) => {
+      const currentPage = i + 1;
+      createPage({
+        path: createPagePath({ node_locale: type.node_locale, type, page: currentPage }),
+        component: resolveTemplatePath(
+          path.resolve('./src/templates/ArticleList.js'),
+          require.resolve('@spin-dd/gatsby-theme-spin-one/src/templates/ArticleList.tsx'),
+        ),
+        context: {
+          locales: allLocales,
+          // TODO: I/F検討
+          name: 'ArticleListPage',
+          // Article検索条件
+          locale: type.node_locale,
+          type: type.slug,
+          sort: [
+            {
+              publishDate: 'DESC',
+            },
+          ],
+          // ページネーション用
+          limit: articlesPerPage,
+          skip: i * articlesPerPage,
+          currentPage,
+          basePath: createPagePath({ node_locale: type.node_locale, type, page: 1 }),
+          // customToggleButton 用
+          pagePath: createCanonicalPath({ type, page: currentPage }),
+        },
+      });
+    });
+
+    // ArticleCategory[]をループして記事一覧ページを生成する
     for (const category of categories) {
+      // locale を含まない記事ページの pagePath を生成
+      const createCanonicalPath = ({ type, category, page }) => {
+        const basePath = `/${type.slug}/${category.slug}/`;
+        return page === 1 ? basePath : `${basePath}${page}/`;
+      };
+      const createPagePath = ({ node_locale, type, category, page }) =>
+        `${resolveLocalePath(node_locale, defaultLocaleCode)}${createCanonicalPath({
+          type,
+          category,
+          page,
+        })}`;
+
       const articles = await graphql(`
         {
           allContentfulArticle(
