c0165f74b7cb36bd0abfdcc1cc63de9b8447d209
KUROSHITA Hideaki <hkuroshi@gmail.com>
2025-01-13 18:10:27 +0900

    feat: enhance ArticleList component with improved type definitions for better type safety

--- packages/gatsby-theme-spin-one/src/components/ArticleList.tsx
+++ packages/gatsby-theme-spin-one/src/components/ArticleList.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import { prepareForParse, parseHtmlToReact } from '../utils';
 import { Layout } from './Layout';
+import type { PageProps } from 'gatsby';
 
 export { Head } from './Head';
 
-export const ArticleList = ({ data, pageContext }) => {
+export const ArticleList: React.FC<
+  PageProps<
+    {
+      template: Queries.ContentfulTemplate;
+      allContentfulArticle: Pick<Queries.ContentfulArticleConnection, 'nodes' | 'pageInfo'>;
+    } & Queries.AllContentfulEntryFragmentFragment
+  >
+> = ({ data, pageContext }) => {
+  const { template } = data;
   // 指定したテンプレートが存在しない場合
-  if (!data.contentfulTemplate) {
+  if (!template) {
     return null;
   }
 
   const { htmlBody, htmlScript, componentData } = prepareForParse({
-    template: data.contentfulTemplate,
+    template,
     data,
     pageContext: {
       ...pageContext,
