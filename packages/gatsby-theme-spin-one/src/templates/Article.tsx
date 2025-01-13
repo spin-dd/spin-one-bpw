5e9076e67c2396570c936a38f01d662ad1db5cbc
KUROSHITA Hideaki <hkuroshi@gmail.com>
2025-01-13 18:10:20 +0900

    feat: enhance ArticleDetail and Article templates with improved type definitions for better type safety

--- packages/gatsby-theme-spin-one/src/templates/Article.tsx
+++ packages/gatsby-theme-spin-one/src/templates/Article.tsx
@@ -1,11 +1,8 @@
-import React from 'react';
 import { graphql } from 'gatsby';
 import { ArticleDetail } from '../components/ArticleDetail';
 
 export { Head } from '../components/ArticleDetail';
-
-export default ({ data, pageContext }) => <ArticleDetail data={data} pageContext={pageContext} />;
-
+export default ArticleDetail;
 export const query = graphql`
   query ($name: String, $locale: String, $slug: String, $spaceId: String) {
     contentfulArticle(slug: { eq: $slug }, node_locale: { eq: $locale }, spaceId: { eq: $spaceId }) {
@@ -14,7 +11,7 @@ export const query = graphql`
       node_locale
       ...ContentfulArticleFieldsFragment
     }
-    contentfulTemplate(name: { eq: $name }, node_locale: { eq: $locale }, spaceId: { eq: $spaceId }) {
+    template: contentfulTemplate(name: { eq: $name }, node_locale: { eq: $locale }, spaceId: { eq: $spaceId }) {
       contentful_id
       __typename
       name
